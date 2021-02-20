a705cc3e889077770d336e742036de64d57b1517
Jon Allen <jallen_swrx@yahoo.com>
2021-02-19 20:53:17 -0500

    Changes to be committed: 	modified:   weather_obs.py
      some correction to --cut after testing

--- weather_obs.py
+++ weather_obs.py
@@ -321,8 +321,10 @@ if __name__ == "__main__":
             # every hour check to see if need to cut
             print("Num minutes running: ", run_minutes )
             if ( cut_file == True):
-                cut_time = datetime.datetime.now()
-                if (cut_time.day > t_begin.day):
+                t_cut_time = datetime.datetime.now()
+#               if (t_cut_time.day > t_begin.day):
+#               if (t_begin.hour - t_cut_time.hour == 4):  
+                if (t_begin.hour):    #testing
                     if ((t_begin.minute - t_cut_time.minute) < 5):
                         print(" cut time less than 1 hour")
                         job1.run()
