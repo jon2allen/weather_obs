ee6005139740de0737794cdbf1cd36061e3ebd5c
Jon Allen <jallen_swrx@yahoo.com>
2021-02-22 22:54:26 -0500

    lambda handler.
      A handler to take data and write it to a EFS file share.
    
      - had to setup a VPC with NAT
      - also had to setup mount points for all avail zones.
      - file also show debugging tech.

---
+++ lambda_handler.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+def lambda_handler(event, context):
+    # TODO implement
+    os.environ['TZ'] = 'US/Eastern'
+    # print(os.getcwd())
+    exec_dir = '/var/task' 
+    os.chdir('/mnt/csv')
+    # os.chdir('/tmp')
+    du = os.popen('df -h').readlines()
+    for f in du:
+        print("Fs:  ", f)
+    print("executing script")
+    exec = "python " + exec_dir + "/weather_obs.py"
+    station = " --station https://w1.weather.gov/xml/current_obs/KDCA.xml"
+    cmd = exec + station
+    os.system(cmd)
+    print("after command")
+    t = os.listdir()
+    for i in t:
+        print("file: ", i)
+    print('Hello from jontest1')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
