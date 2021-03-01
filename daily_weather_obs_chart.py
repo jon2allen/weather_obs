1d91ff65e8e25108dec8cd6f00f08c1d1b74cf33
Jon Allen <jallen_swrx@yahoo.com>
2021-02-28 22:19:41 -0500

    chart genrator

--- daily_weather_obs_chart.py
+++ daily_weather_obs_chart.py
@@ -12,7 +12,11 @@ import argparse
 import csv
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
 import time
+import datetime
 # station is 4 character NOAA observation station in CAPS
 # csv dir is where the data resides
 # where the graph png shoud be placed.
@@ -21,6 +25,8 @@ csv_dir = ""
 graph_out_dir = ""
 day = "16"
 month = "02"
+now = datetime.datetime.now()
+
 
 """
   code logic
@@ -34,6 +40,7 @@ station = "KDCA"
 
 dirlist = os.listdir()  
 station_file_list = []
+target_csv = ""
 
 file_id = station + "_Y"
 
@@ -47,7 +54,58 @@ for f in station_file_list:
     print("Month: ", f[12:14])
     print("day: ", f[16:18])
     d1 = int(f[16:18])
-    if (f[16:18] == day):
+    if (d1 == int(now.day)):
         print("Match day:", f)
+        target_csv = f
     if  d1 < int(day):
         print("In the past:" ,f)
+        
+        
+obs1 = pd.read_csv(target_csv,parse_dates=[9])
+
+print(obs1.shape)
+print(obs1.columns)
+
+obs1['wind_gust_mph'] = pd.to_numeric(obs1['wind_gust_mph'], errors = 'coerce')
+obs1['wind_mph'] = pd.to_numeric(obs1['wind_mph'], errors = 'coerce')
+# obs1['observation_time'] = obs1['observation_time'].dt.hour
+
+print(obs1.dtypes)
+
+
+# rows, cols = wind.shape
+positions = []
+labels = []
+fig, ax = plt.subplots()
+fig.set_size_inches(12,6)
+
+
+x = obs1['observation_time']
+y = obs1['wind_mph']
+z = obs1['wind_dir']
+
+ax.plot_date( x,y, linestyle = "solid")
+plt.grid(True)
+plt.title("National Airport", fontsize=14)
+for  i in range(  x.size  ):
+     if (i == (x.size - 1 )):
+             ax.annotate(z[i], (mdates.date2num(x[i]), y[i]), xytext=(-15, -15), 
+               textcoords='offset points')
+     else:
+           if y[i] == y[i+1]:
+                ax.annotate(z[i], (mdates.date2num(x[i]), y[i]), xytext=(-30, 15), 
+                   textcoords='offset points')
+           else:
+                ax.annotate(z[i], (mdates.date2num(x[i]), y[i]), xytext=(-15, -15), 
+                    textcoords='offset points')
+
+fig.autofmt_xdate()
+fig.text(0.04,0.5, 'Wind Speed - MPH', va='center', rotation='vertical', fontsize=18)
+fig.text(0.5,0.05,  'Hour of day', va='center', fontsize=18)
+# plt.xticks(positions,labels)
+date_form = DateFormatter("%I")
+ax.xaxis.set_major_formatter(date_form)
+fig.savefig(station + '_current'+ '.png', dpi=fig.dpi)
+print(positions)
+print(labels)
+        
