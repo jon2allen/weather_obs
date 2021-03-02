bcf63eccde713dd2e3c6633b84e15f21b79e3903
EC2 Default User <ec2-user@ip-172-31-94-92.ec2.internal>
2021-03-02 18:58:08 -0500

    working version of daily weather chart

--- daily_weather_obs_chart.py
+++ daily_weather_obs_chart.py
@@ -17,15 +17,21 @@ import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
 import time
 import datetime
+import dateutil
 # station is 4 character NOAA observation station in CAPS
 # csv dir is where the data resides
 # where the graph png shoud be placed.
+os.environ['TZ'] = 'US/Eastern'
+os.chdir('/var/www/html/weather_obs')
 station = ""
 csv_dir = ""
 graph_out_dir = ""
-day = "16"
-month = "02"
 now = datetime.datetime.now()
+
+day = str(now.day)
+month = str(now.month)
+print("month: ", month )
+print("day: ", day )
 
 
 """
@@ -42,26 +48,32 @@ dirlist = os.listdir()
 station_file_list = []
 target_csv = ""
 
-file_id = station + "_Y"
+file_id = station + "_Y" + str(now.year)
+
+print("file_id:", file_id )
+
 
 for f in dirlist:
-    if( f[:6] == file_id):
+    if( f[:10] == file_id):
        print("station CSV file:" ,f)
        station_file_list.append(f)
-    print("file: ", f[:6])
+    print("file: ", f[:10])
 print(station_file_list)    
 for f in station_file_list:
-    print("Month: ", f[12:14])
-    print("day: ", f[16:18])
-    d1 = int(f[16:18])
-    if (d1 == int(now.day)):
-        print("Match day:", f)
-        target_csv = f
-    if  d1 < int(day):
-        print("In the past:" ,f)
+    m1 = int(f[12:14])
+    if m1 == int(now.month):
+       print("match month")
+       print("Month: ", f[12:14])
+       print("day: ", f[16:18])
+       d1 = int(f[16:18])
+       if (d1 == int(now.day)):
+           print("Match day:", f)
+           target_csv = f
+       if  d1 < int(day):
+           print("In the past:" ,f)
         
-        
-obs1 = pd.read_csv(target_csv,parse_dates=[9])
+date_utc = lambda x: dateutil.parser.parse(x, ignoretz=True)
+obs1 = pd.read_csv(target_csv,parse_dates=[9],date_parser=date_utc)
 
 print(obs1.shape)
 print(obs1.columns)
@@ -87,7 +99,7 @@ z = obs1['wind_dir']
 ax.plot_date( x,y, linestyle = "solid")
 plt.grid(True)
 plt.title("National Airport", fontsize=14)
-for  i in range(  x.size  ):
+for  i in range(  x.size   ):
      if (i == (x.size - 1 )):
              ax.annotate(z[i], (mdates.date2num(x[i]), y[i]), xytext=(-15, -15), 
                textcoords='offset points')
@@ -99,6 +111,7 @@ for  i in range(  x.size  ):
                 ax.annotate(z[i], (mdates.date2num(x[i]), y[i]), xytext=(-15, -15), 
                     textcoords='offset points')
 
+
 fig.autofmt_xdate()
 fig.text(0.04,0.5, 'Wind Speed - MPH', va='center', rotation='vertical', fontsize=18)
 fig.text(0.5,0.05,  'Hour of day', va='center', fontsize=18)
@@ -106,6 +119,8 @@ fig.text(0.5,0.05,  'Hour of day', va='center', fontsize=18)
 date_form = DateFormatter("%I")
 ax.xaxis.set_major_formatter(date_form)
 fig.savefig(station + '_current'+ '.png', dpi=fig.dpi)
-print(positions)
-print(labels)
-        
+print(x)
+print(y)
+print(z)
+print(x.size)
+print(y.size)        
