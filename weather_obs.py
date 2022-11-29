407e0f92d45acc30ec9f0510ecf35bbd1cdd2584
Jon Allen <jallen_swrx@yahoo.com>
2022-11-29 10:17:57 -0500

    writing obs data  must be same day

--- weather_obs.py
+++ weather_obs.py
@@ -758,7 +758,10 @@ def weather_collect_driver(obs1):
     obs1.current_obs_time = get_obs_time(outdata[1][9])
     obs1.local_time = ObsDate( outdata[1][9])
     obs1.current_local_time = obs1.local_time.local_now_reg()
-    trace_print(4, "Local time now: ", str(obs1.current_local_time))    
+    trace_print(4, "Local time now: ", str(obs1.current_local_time)) 
+    if obs1.current_obs_time.day != obs1.current_local_time.day:
+        trace_print(4, "obs time not on same day, exiting... ", str(obs1.current_obs_time))
+        return False
     if (obs1.prior_obs_time.hour == 23):
         trace_print(4, "Special driver processing at hour 23")
         obs1.prior_obs_time = obs1.current_obs_time
