adb9525fa2a14f531605c8c11e58d22b693a9676
jallen <jallen_swrx@yahoo.com>
2023-04-24 13:23:16 -0400

    get ObsCollector from obs_3_day_collect

--- noaa_tide_chart_puller.py
+++ noaa_tide_chart_puller.py
@@ -23,128 +23,20 @@ import obs_utils
 from weather_obs import create_station_file_name
 from io import StringIO
 import pandas as pd
+from obs_3_day_collect import ObsCollector
 #from freezegun import freeze_time
 #import unittest
 
 #freezer = freeze_time("2021-12-30 23:56:30", tick=True)
 #freezer.start()
 
-class ObsCollector:
-    def __init__(self, url, id):
-        self.station_url = url
-        self.station_id = id
-
-    def show_collector(self):
-        return str(self.station_id + "@" + self.station_url)
-
-    def get_url_data(self):
-        self.url_data = requests.get(self.station_url)
-
-    def show_url_data(self):
-        """show Url data if desired"""
-        return self.url_data.text
-
-    def set_station_file_name(self):
-        self.obs_filename = create_station_file_name(self.station_id, "txt")
-
-    def write_station_data(self):
-        """write out last forcast"""
-        obs_file = open(self.obs_filename, "w")
-        obs_file.write(self.last_forcast)
-        obs_file.close()
-
-    def write_station_data_custom(self, ending_text):
-        obs_file = open(self.station_id + "_" + ending_text, "w")
-        obs_file.write(self.last_forcast)
-        obs_file.close()
-
-    def obs_collection_sequence(self):
-        """ basic collection sequence - fetch, intpret, write """
-        self.get_url_data()
-        self.find_station_data()
-        self.set_station_file_name()
-        if (self.obs_collection_duplicate_check()):
-            print("duplicate:  exit")
-        else:
-            self.write_station_data()
-
-    def obs_collection_duplicate_check(self):
-        # note to get correct md5 checksum - binary file comparison
-        # initialz test_md5 - preven error on fresh dir
-        test_md5 = " "
-        self.write_station_data_custom("dupcheck.txt")
-        with open(self.station_id + "_" + "dupcheck.txt", "rb") as fl:
-            blob1 = fl.read()
-            curr_md5 = str(hashlib.md5(blob1).hexdigest())
-            print("Curr_md5:", curr_md5)
-        today = datetime.now()
-        day_1 = timedelta(hours=24)
-        yesterday = today - day_1
-        today_glob = obs_utils.create_station_glob_filter(
-            self.station_id, "txt", today)
-        yesterday_glob = obs_utils.create_station_glob_filter(
-            self.station_id, "txt", yesterday)
-        last_file = obs_utils.hunt_for_noaa_files2(".", today_glob)
-        if len(last_file) < 1:
-            last_file = obs_utils.hunt_for_noaa_files2(".", yesterday_glob)
-        if (len(last_file) > 0):
-            with open(last_file, "rb") as f2:
-                blob2 = f2.read()
-                test_md5 = str(hashlib.md5(blob2).hexdigest())
-                print("test_md5", test_md5)
-                print("test_file ", last_file)
-        return (curr_md5 == test_md5)
-
-    def find_station_data(self):
-        """ parse station data from noaa html page """
-        f = BeautifulSoup(self.url_data.text, 'lxml')
-        my_content = f.find(id="contentarea")
-        # print(my_content)
-        t = re.finditer(r'\w\w\w\d\d\d\W\d\d\d\d\d\d',  str(my_content))
-        station_list = []
-        text_obs = str(my_content)
-        #leantext = BeautifulSoup(text_obs,'lxml').text
-        #text_obs = leantext
-        for i in t:
-            index = int(i.start())
-            # print(str(index))
-            # print(text_obs[index:(index+6)])
-            station_name = text_obs[index:(index+6)]
-            station_list.append((index, station_name))
-        tail = re.finditer(r'Start Tail', str(my_content))
-        for s in tail:
-            # print("Start Tail: ", str(s.start()))
-            station_list.append((int(s.start() - 5), 'end'))
-        print(station_list)
-        print(len(station_list))
-        station_list_len = len(station_list)
-        # print("test_obs:")
-        # print(text_obs)
-
-        for i in range(station_list_len):
-            station_data = "<not found>"
-            if i == (station_list_len - 1):
-                break
-            #print("i: ", str(i) )
-            station_data = text_obs[station_list[i][0]:station_list[i+1][0]]
-            #print("station_data:", station_data)
-            #print(" self.station:", self.station_id)
-            if self.station_id in station_data:
-                my_station = station_data
-                break
-        r_station_data = BeautifulSoup(station_data, 'lxml').text
-        self.last_forcast = r_station_data
-        print(hashlib.md5(r_station_data.encode('utf-8')).hexdigest())
-        return r_station_data
-
-#
 #  init the collector
 #  run the collection sequnce
 #  1 additional custom write out of the data
 
 class ObsTideCollector( ObsCollector):
-    def __init__( self, url, id ):
-        super().__init__(url, id)
+    def __init__( self, url, id, filetype ):
+        super().__init__(url, id, filetype )
         # use cache on disk first
         # need test for end of year
         # may need to get this year and next.
@@ -169,7 +61,7 @@ class ObsTideCollector( ObsCollector):
             self.obs_filename = f_list.pop()
             self.load_tidal_data(self.obs_filename)
             self.read_next_year_data()
-    def find_station_data(self):
+    def _find_station_data(self):
         self.last_forcast = str(self.url_data.text)
         return self.url_data
     def obs_collection_duplicate_check(self):
@@ -240,7 +132,7 @@ class ObsTideCollector( ObsCollector):
         self.station_url = self.station_url.replace(bstring,nstring)
         print("url: ", self.station_url)
         self.get_url_data()
-        self.find_station_data()
+        self._find_station_data()
         self.write_station_data_custom("Y"+str(next_year)+".txt")
     def read_next_year_data(self):
         today = datetime.now()
@@ -302,7 +194,7 @@ if __name__ == "__main__":
 # The script changes wording directory and then writes
 # DATA_DIR
 #######################################################################
-    TIDECHARTURL = 'https://tidesandcurrents.noaa.gov/cgi-bin/predictiondownload.cgi?&stnid=8594900&threshold=&thresholdDirection=greaterThan&bdate=2021&timezone=LST/LDT&datum=MLLW&clock=12hour&type=txt&annual=true'
+    TIDECHARTURL = 'https://tidesandcurrents.noaa.gov/cgi-bin/predictiondownload.cgi?&stnid=8594900&threshold=&thresholdDirection=greaterThan&bdate=2023&timezone=LST/LDT&datum=MLLW&clock=12hour&type=txt&annual=true'
     FORECASTID = 'alex'
     DATA_DIR = '/var/www/html/weather_obs/data'
     DEBUG=False
@@ -319,7 +211,7 @@ if __name__ == "__main__":
     import logging
     logger = logging.getLogger('weather_obs_f')
 
-    obs_t = ObsTideCollector(url=TIDECHARTURL, id=FORECASTID)
+    obs_t = ObsTideCollector(url=TIDECHARTURL, id=FORECASTID, filetype="txt")
     print(obs_t.show_collector())
 
 
